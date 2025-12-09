f2bf1bc53f00edd25af6806f6aa011f8283fb818
MaxS2027 <max.schwartz2027@gbwl.org>
2025-12-09 16:05:23 +0000

    3.3 Notes

--- Main.java
+++ Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      //Create a string short cut
+      String Lebron = "GOAT";
+      //Strings are actually objects
+      //OG way to make string
+      String steph = new string(""GOAT"");
+
+      //ALL objects can be created using this pattern
+      //ClassName objectName = New Classname();
+      buildABear kitty = new buildABear("Cat");
    }
 }
